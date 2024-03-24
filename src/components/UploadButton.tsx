53d395ccc7e7e5eda68b8f16cb87e9c89c398e42
beenow011 <abhinavnb11@gmail.com>
2024-03-24 12:47:50 +0530

    upload button 1

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -5,13 +5,45 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import DropZone from 'react-dropzone'
+import { Cloud, File } from "lucide-react";
 
+const UploadZone = () => {
+    return (<DropZone multiple={false} onDrop={(acceptedFiles) => { }}>
+        {({ getRootProps, getInputProps, acceptedFiles }) => (
+            <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
+                <div className="flex items-center justify-center h-full w-full">
+                    <label htmlFor="dropZone-file" className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                            <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                            <p className="mb-2 text-sm text-zinc-700">
+                                <span className="font-semibold">Click to upload</span> or drag and drop
+                            </p>
+                            <p className="text-sm text-zinc-500">PDF (up to 4MB)</p>
+                        </div>
+                        {acceptedFiles && acceptedFiles[0] && (
+                            <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                                <div className="px-3 py-2 h-full grid place-items-center">
+                                    <File className='h-4 w-4 text-blue-500' />
+                                </div>
+                                <div className="px-3 py-2 h-full text-sm truncate">{acceptedFiles[0].name}</div>
+                            </div>
+                        )}
+                    </label>
+                </div>
+            </div>
+        )}
+    </DropZone>)
+}
 const UploadButton = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <Dialog open={isOpen} onOpenChange={(v) => { !v && setIsOpen(v) }}>
             <DialogTrigger onClick={() => setIsOpen(true)} asChild><Button> Upload PDF</Button></DialogTrigger>
-            <DialogContent>Hey Abhi</DialogContent>
+            <DialogContent>
+                <UploadZone />
+
+            </DialogContent>
         </Dialog>
     )
 }
