5c41fb9a55fcc52730a504de1ec76f62ec0de254
beenow011 <abhinavnb11@gmail.com>
2024-03-25 15:54:00 +0530

    Zooming the PDF

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import { ChevronDown, ChevronUp, Loader2, Search } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
@@ -13,6 +13,9 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { cn } from '@/lib/utils';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import SimpleBar from "simplebar-react"
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -24,6 +27,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
     const [numPages, setNumPages] = useState<number>()
     const [currPage, setCurrPage] = useState(1)
+    const [scale, setScale] = useState(1)
     const { toast } = useToast();
     const { width, ref } = useResizeDetector()
 
@@ -75,31 +79,58 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                     <ChevronUp className='h-4 w-4' />
                 </Button>
             </div>
-            <div></div>
+            <div className='space-x-2'>
+                <DropdownMenu >
+                    <DropdownMenuTrigger asChild>
+                        <Button className='gap-1.5' aria-label='zoom' variant='ghost'>
+                            <Search className='h-4 w-4' />
+                            {scale * 100}%
+                            <ChevronDown className='h-3 w-3 opacity-50' />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onSelect={() => setScale(1)}>
+                            100%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                            150%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(2)}>
+                            200%
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                            250%
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
         <div className="flex-1 w-full max-h-screen ">
-            <div ref={ref}>
-                <Document loading={
-                    <div className='flex justify-center'>
-                        <Loader2 className='my-24 h-6 w-6 animate-spin' />
-                    </div>
-                }
-                    onLoadError={() => {
-                        toast({
-                            title: 'Error loading PDF!',
-                            description: 'Please try again',
-                            variant: 'destructive'
-                        })
-                    }}
-                    onLoadSuccess={({ numPages }) => {
-                        setNumPages(numPages)
-                    }}
-                    file={url} className='max-h-full'>
-                    <Page pageNumber={currPage} width={width ? width : 1} />
+            <SimpleBar autoHide={false} className='max-h-[calc(100vh-10rem)]'>
+                <div ref={ref}>
+                    <Document loading={
+                        <div className='flex justify-center'>
+                            <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                        </div>
+                    }
+                        onLoadError={() => {
+                            toast({
+                                title: 'Error loading PDF!',
+                                description: 'Please try again',
+                                variant: 'destructive'
+                            })
+                        }}
+                        onLoadSuccess={({ numPages }) => {
+                            setNumPages(numPages)
+                        }}
+                        file={url} className='max-h-full'>
+                        <Page pageNumber={currPage} scale={scale} width={width ? width : 1} />
 
-                </Document>
-            </div>
+                    </Document>
+                </div>
+            </SimpleBar>
         </div >
+
     </div >)
 }
 export default PdfRenderer 
