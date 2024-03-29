36e0db68645bc888cc7e50f4e1b74a0c15fb6aa8
beenow011 <abhinavnb11@gmail.com>
2024-03-29 10:52:52 +0530

    showing chat as soon as it it sent

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,6 +1,8 @@
-import React, { ReactNode, createContext, useState } from "react";
+import React, { ReactNode, createContext, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResonponse = {
     addMessage: () => void,
@@ -26,6 +28,9 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
     const [message, setmessage] = useState('')
     const [isLoading, setIsloading] = useState(false)
     const { toast } = useToast();
+    const backupMessage = useRef('')
+    const utils = trpc.useContext()
+
     const { mutate: sendMessage } = useMutation({
         mutationFn: async ({ message }: { message: string }) => {
             console.log("1", message)
@@ -41,6 +46,49 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
             }
 
             return response.body
+        },
+        onMutate: async ({ message }) => {
+            backupMessage.current = message
+            setmessage('')
+
+            await utils.getFileMessages.cancel()
+            const previousMessages = utils.getFileMessages.getInfiniteData()
+            utils.getFileMessages.setInfiniteData(
+                {
+                    fileId,
+                    limit: INFINITE_QUERY_LIMIT
+                },
+                (old) => {
+                    if (!old) {
+                        return {
+                            pages: [],
+                            pageParams: []
+                        }
+
+                    }
+                    let newPages = [...old.pages]
+                    let latestPage = newPages[0]!
+                    latestPage.messages = [
+                        {
+                            createdAt: new Date().toISOString(),
+                            id: crypto.randomUUID(),
+                            text: message,
+                            isUserMessage: true
+                        },
+                        ...latestPage.messages
+                    ]
+                    newPages[0] = latestPage
+
+                    return {
+                        ...old,
+                        pages: newPages
+                    }
+                }
+            )
+            setIsloading(true)
+            return {
+                previousMessages: previousMessages?.pages.flatMap(page => page.messages) ?? []
+            }
         }
     })
 
