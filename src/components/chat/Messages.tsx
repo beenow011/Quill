f9edbfcab9af72d7911d150cb27db3a3c55e1d61
beenow011 <abhinavnb11@gmail.com>
2024-03-29 15:50:16 +0530

    reference and infinite query

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -3,9 +3,9 @@ import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
 import { Loader2, MessageSquare } from "lucide-react"
 import Skeleton from "react-loading-skeleton"
 import Message from "./Message"
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { ChatContext } from "./ChatContext"
-
+import { useIntersection } from '@mantine/hooks'
 interface msgProps {
     fileId: string
 }
@@ -41,10 +41,21 @@ const Messages = ({ fileId }: msgProps) => {
     }
 
     const combinedMessages = [
-        ...(true ? [loadingMessage] : []),
+        ...(isAIthinking ? [loadingMessage] : []),
         ...(messages ?? []),
     ]
 
+    const lastMessageRef = useRef<HTMLDivElement>(null)
+
+    const { ref, entry } = useIntersection({
+        root: lastMessageRef.current,
+        threshold: 1
+    })
+    useEffect(() => {
+        if (entry?.isIntersecting) {
+            fetchNextPage()
+        }
+    }, [entry, fetchNextPage])
     return (
         <div className="flex max-h-[calc(100vh-3rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch ">
 
@@ -60,7 +71,7 @@ const Messages = ({ fileId }: msgProps) => {
                                 return (
                                     <Message
                                         key={message.id}
-
+                                        ref={ref}
                                         message={message}
                                         isNextMessageSamePerson={
                                             isNextMessageSamePerson
