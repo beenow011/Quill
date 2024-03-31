e5ba8d776bd0d0de9c949ddbd06b6eaab35431c9
beenow011 <abhinavnb11@gmail.com>
2024-03-31 12:04:44 +0530

    vercel error

--- src/lib/uploadThing.ts
+++ src/lib/uploadThing.ts
@@ -1,7 +1,6 @@
-// import { OurFileRouter } from "@/app/api/uploadthing/core";
-import { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateReactHelpers } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
  
+import type { OurFileRouterType } from "@/app/api/uploadthing/core";
  
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
+export const { useUploadThing } =
+  generateReactHelpers<OurFileRouterType>();
