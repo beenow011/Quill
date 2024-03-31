268879105f31d206ce4ad731c113f7ea557893dd
beenow011 <abhinavnb11@gmail.com>
2024-03-31 11:46:16 +0530

    build error2

--- src/lib/uploadThing.ts
+++ src/lib/uploadThing.ts
@@ -1,3 +1,4 @@
+// import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react";
  
