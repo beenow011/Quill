ce4bdfeb960b476585776b0c78fb87754a6dd103
beenow011 <abhinavnb11@gmail.com>
2024-03-31 16:54:47 +0530

    vervel 2

--- src/lib/uploadThing.ts
+++ src/lib/uploadThing.ts
@@ -1,6 +1,6 @@
 import { generateReactHelpers } from "@uploadthing/react/hooks";
  
-import type { OurFileRouterType } from "@/app/api/uploadthing/core";
+import type { OurFileRouterType } from "@/app/api/uploadthing/core.ts";
  
 export const { useUploadThing } =
   generateReactHelpers<OurFileRouterType>();
