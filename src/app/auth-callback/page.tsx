27ecb25bc66d51bf5362a30e4885e8dbb64fc468
beenow011 <abhinavnb11@gmail.com>
2024-04-01 07:26:39 +0530

    authcallback error

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation"
-import { trpc } from "../_trpc/client"
-import { Loader2 } from "lucide-react"
+"use client"
 
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
 
 const Page = () => {
     const router = useRouter()
@@ -10,39 +10,33 @@ const Page = () => {
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
 
-
-
-    const query = trpc.authCallback.useQuery(undefined, {
+    trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({ success }) => {
+            if (success) {
+                // user is synced to db
+                router.push(origin ? `/${origin}` : '/dashboard')
+            }
+        },
+        onError: (err) => {
+            if (err.data?.code === 'UNAUTHORIZED') {
+                router.push('/sign-in')
+            }
+        },
         retry: true,
         retryDelay: 500,
-    });
-
-    // Check for errors in the query result
-    if (query.error) {
-        const errData = query.error.data;
-        if (errData?.code === 'UNAUTHORIZED') {
-            router.push('/sign-in');
-        } else {
-            // Handle other types of errors
-            console.error("An error occurred:", query.error);
-        }
-    }
-
-    // Continue with other logic based on the query result
-    if (query.data?.success) {
-        router.push(origin ? `/${origin}` : '/dashboard');
-    }
+    })
 
     return (
-        <div className="w-full mt-24 flex justify-center">
-            <div className="flex flex-col items-center gap-2">
-                <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-                <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <div className='w-full mt-24 flex justify-center'>
+            <div className='flex flex-col items-center gap-2'>
+                <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+                <h3 className='font-semibold text-xl'>
+                    Setting up your account...
+                </h3>
                 <p>You will be redirected automatically.</p>
             </div>
         </div>
     )
-
 }
 
 export default Page
