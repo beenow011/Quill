63b5bc1b2efdc5ab881936e2c8b7866d003799dc
beenow011 <abhinavnb11@gmail.com>
2024-04-01 20:45:20 +0530

    uploadThing error

--- src/lib/uploadThing.ts
+++ src/lib/uploadThing.ts
@@ -1,6 +1,6 @@
-import { OurFileRouter } from "@/app/api/uploadthing/core";
+// import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react/hooks";
  
  
 export const { useUploadThing } =
-generateReactHelpers<OurFileRouter>()
+generateReactHelpers()
