e51eb745f3af17ecb70548316cf333311298cb83
beenow011 <abhinavnb11@gmail.com>
2024-04-02 19:56:09 +0530

    suspense use params

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 // import { trpc } from '../_trpc/client';
@@ -7,12 +7,15 @@ import { Loader2 } from 'lucide-react';
 import { trpc } from '../_trpc/client';
 
 const Page = () => {
+
+    function SearchBarFallback() {
+        return <>placeholder</>
+    }
     const router = useRouter()
+
 
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
-
-
 
     const query = trpc.authCallback.useQuery(undefined, {
         retry: true,
@@ -34,15 +37,26 @@ const Page = () => {
     if (query.data?.success) {
         router.push(origin ? `/${origin}` : '/dashboard');
     }
+
+
+
+
+
+
+
+
+
     return (
         <div className='w-full mt-24 flex justify-center'>
-            <div className='flex flex-col items-center gap-2'>
-                <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-                <h3 className='font-semibold text-xl'>
-                    Setting up your account...
-                </h3>
-                <p>You will be redirected automatically.</p>
-            </div>
+            <Suspense fallback={<SearchBarFallback />}>
+                <div className='flex flex-col items-center gap-2'>
+                    <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+                    <h3 className='font-semibold text-xl'>
+                        Setting up your account...
+                    </h3>
+                    <p>You will be redirected automatically.</p>
+                </div>
+            </Suspense>
         </div>
     );
 };
