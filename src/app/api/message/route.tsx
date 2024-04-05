907f2113de4410066b366df0b6f2c556af10032b
beenow011 <abhinavnb11@gmail.com>
2024-04-05 14:47:14 +0530

    error solved

--- src/app/api/message/route.tsx
+++ src/app/api/message/route.tsx
@@ -1,27 +1,28 @@
-import { db } from "@/db";
-import { openai } from "@/lib/openai";
-import { getPineconeClient } from "@/lib/pinecone";
-import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { PineconeStore } from "@langchain/pinecone";
-import { NextRequest } from "next/server";
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { db } from '@/db';
+import { openai } from '@/lib/openai';
+import { getPineconeClient } from '@/lib/pinecone';
+// import { SendMessageValidator } from '@/lib/SendMessageValidator';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PineconeStore } from '@langchain/pinecone';
+import { NextRequest } from 'next/server';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { SendMessageValidator } from '@/lib/validators/SendMessageValidator';
 
 interface SearchResult {
     pageContent: string;
     metadata: {
-        fileName: string;
+        fileId: string;
     };
 }
 
 function customFilter(result: SearchResult, targetFileName: string): boolean {
-    return result.metadata?.fileName === targetFileName;
+    console.log("result", result)
+    return result.metadata?.fileId === targetFileName;
 }
 
 export const POST = async (req: NextRequest) => {
-    const body = await req.json()
-
+    const body = await req.json();
     const { getUser } = getKindeServerSession();
     const user = await getUser()
     // const { id: userId } = user?
@@ -31,17 +32,13 @@ export const POST = async (req: NextRequest) => {
         return new Response('Unauthorized', { status: 401 })
     }
 
-    const { fileId, message } = SendMessageValidator.parse(body)
-    console.log("2", message)
-
-    const file = await db.file.findFirst({
-        where: {
-            id: fileId,
-            userId: user.id
-        }
-    })
+    const { fileId, message } = SendMessageValidator.parse(body);
+    // console.log("file", fileId)
+    // console.log("message", message)
+    const file = await db.file.findFirst({ where: { id: fileId, userId: user.id } });
+    console.log(file?.name)
     if (!file) {
-        return new Response('Not found', { status: 404 })
+        return new Response('Not found', { status: 404 });
     }
 
     await db.message.create({
@@ -49,72 +46,78 @@ export const POST = async (req: NextRequest) => {
             text: message,
             isUserMessage: true,
             userId: user.id,
-            fileId
-        }
-    })
-
-    const pinecone = await getPineconeClient()
-    // console.log(2)
-
-    const pineconeIndex = pinecone.Index("quill")
-    const embeddings = new OpenAIEmbeddings({
-        openAIApiKey: process.env.OPENAI_API_KEY
-    })
-
-    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, { pineconeIndex })
-
-
-    const results = await vectorStore.similaritySearch(message, 1, {
-        filter: (result: SearchResult) => customFilter(result, file.id)
-    });
-
-    const prevMessage = await db.message.findMany({
-        where: {
-            fileId
+            fileId,
         },
-        orderBy: {
-            createdAt: "asc"
-        },
-        take: 6
     })
 
-    const formattedMessages = prevMessage.map(msg => ({
-        role: msg.isUserMessage ? "user" as const : "assistant" as const,
-        content: msg.text
-    }))
+    // 1: Vectorize message
+    const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY });
 
-    const context = `PREVIOUS CONVERSATION:${formattedMessages.map((msg) => {
-        if (msg.role === 'user') return `User:${msg.content}\n`;
-        return `Assistant:${msg.content}\n`;
-    })}CONTEXT:${results.map((r) => r.pageContent).join('\n\n')}USER INPUT:${message}`;
+    // Initialize the Pinecone vector store
+    const pinecone = await getPineconeClient();
+    const pineconeIndex = pinecone.Index('quill'); // Use a single index name
 
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        temperature: 0,
-        stream: true,
-        messages: [
-            {
-                role: 'system',
-                content: 'You have access to a PDF document. Please use the information from the document to answer the user\'s question.',
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, { pineconeIndex });
+
+
+    try {
+        // Search for similar messages using the file ID as context
+        const results = await vectorStore.similaritySearch(message, 4, { fileId });
+        // console.log("res", results)
+
+        // const filteredResult = results.filter(ele => ele.metadata.fileId === fileId)
+        // console.log("filter", filteredResult)
+        const prevMessages = await db.message.findMany({
+            where: { fileId },
+            orderBy: { createdAt: 'asc' },
+            take: 6,
+        });
+        const formattedPrevMessages = prevMessages.map((msg) => ({
+            role: msg.isUserMessage ? 'user' : 'assistant',
+            content: msg.text,
+        }));
+
+        // Construct a context string with previous conversation, results, and user input
+        const context = `PREVIOUS CONVERSATION:${formattedPrevMessages.map((msg) => {
+            if (msg.role === 'user') return `User:${msg.content}\n`;
+            return `Assistant:${msg.content}\n`;
+        })}CONTEXT:${results.map((r) => r.pageContent).join('\n\n')}USER INPUT:${message}`;
+
+
+        // Use a system message to instruct the model
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            temperature: 0.7, // Adjust the temperature as needed
+            stream: true,
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You have access to a PDF document. Please use the information from the document to answer the user\'s question.',
+                },
+                {
+                    role: 'user',
+                    content: context, // Provide the context here
+                },
+            ],
+        });
+
+        const stream = OpenAIStream(response, {
+            async onCompletion(completion) {
+                await db.message.create({
+                    data: {
+                        text: completion,
+                        isUserMessage: false,
+                        fileId,
+                        userId: user.id,
+                    },
+                });
             },
-            {
-                role: 'user',
-                content: context, // Provide the context here
-            },
-        ],
-    })
+        });
+        // console.log(stream)
 
-    const stream = OpenAIStream(response, {
-        async onCompletion(completion) {
-            await db.message.create({
-                data: {
-                    text: completion,
-                    isUserMessage: false,
-                    fileId,
-                    userId: user.id
-                }
-            })
-        }
-    })
-    return new StreamingTextResponse(stream);
-}
+        return new StreamingTextResponse(stream);
+    } catch (error) {
+        console.error('Error searching for similar messages:', error);
+        return new Response('InternalServerError', { status: 500 });
+    }
+};
